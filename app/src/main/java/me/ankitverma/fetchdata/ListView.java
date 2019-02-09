fe318c6ccd7405dd908429f5a2a329b30777473f
Ankit Verma <ankitverma3103@gmail.com>
2019-02-09 20:59:36 +0530

    Initial commit

---
+++ app/src/main/java/me/ankitverma/fetchdata/ListView.java
@@ -0,0 +1,25 @@
+package me.ankitverma.fetchdata;
+
+public class ListView {
+    private String login;
+    private String avatar_url;
+    private String html_url;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public ListView(String login, String gitUrl, String avatar_url) {
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.html_url = gitUrl;
+    }
+}
